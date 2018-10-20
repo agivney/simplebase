75aff4fb88b5b6a108746f03e3b41db7163cb6fa
Arthur Givney <agivney@yahoo.com.au>
2018-10-20 14:51:12 +1100

    removed folder

---
+++ target/debug/deps/simplebase-b7585508bb7b0acb.d
@@ -0,0 +1,8 @@
+/home/pi/Desktop/rust/projects/base/target/debug/deps/libsimplebase-b7585508bb7b0acb.rlib: src/lib.rs src/engine/mod.rs src/file_services/mod.rs src/tests/mod.rs
+
+/home/pi/Desktop/rust/projects/base/target/debug/deps/simplebase-b7585508bb7b0acb.d: src/lib.rs src/engine/mod.rs src/file_services/mod.rs src/tests/mod.rs
+
+src/lib.rs:
+src/engine/mod.rs:
+src/file_services/mod.rs:
+src/tests/mod.rs:

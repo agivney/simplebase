d9a0b2a0801d2a5d87411c271980ad90a174ce98
Arthur <agivney@yahoo.com.au>
2018-10-28 11:21:55 +1000

    removed target directory

--- target/package/simplebase-0.2.2/target/debug/deps/simplebase-ac2c11e07c34c7a4.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Arthur\Desktop\rust\projects\base\target\package\simplebase-0.2.2\target\debug\deps\libsimplebase-ac2c11e07c34c7a4.rlib: src\lib.rs src\engine\mod.rs src\file_services\mod.rs src\tests\mod.rs
-
-C:\Users\Arthur\Desktop\rust\projects\base\target\package\simplebase-0.2.2\target\debug\deps\simplebase-ac2c11e07c34c7a4.d: src\lib.rs src\engine\mod.rs src\file_services\mod.rs src\tests\mod.rs
-
-src\lib.rs:
-src\engine\mod.rs:
-src\file_services\mod.rs:
-src\tests\mod.rs:
